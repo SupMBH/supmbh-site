1ec9a3e93e4c56b1a4d47c588910d9efc43f912e
supmbh <supmbh@gmail.com>
2024-10-18 01:35:30 +0200

    skills1.2

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './Skills.css'; 
+import { Link } from 'react-router-dom';
+import './Skills.css';
 import hardSkillsData from '../data/hardSkills.json';
 
 function HardSkills() {
   return (
     <section className="skills-section">
-      <h2 className="skills-title">Hard Skills</h2>
+      <h2 className="skills-title">Compétences Techniques</h2>
       <div className="gallery">
         {hardSkillsData.map((skill, index) => (
-          <div key={index} className="card" tabIndex="0">
-            <h3>{skill.title}</h3>
-            <p>{skill.description}</p>
-          </div>
+          <Link to={skill.link} key={index} className="card-link">
+            <div className="card" tabIndex="0">
+              <h3>{skill.title}</h3>
+              <p>{skill.description}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </section>
@@ -19,3 +22,4 @@ function HardSkills() {
 }
 
 export default HardSkills;
+
--- src/components/SoftSkills.js
+++ src/components/SoftSkills.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Skills.css';
 import softSkillsData from '../data/softSkills.json';
 
 function SoftSkills() {
   return (
     <section className="skills-section">
-      <h2 className="skills-title">Soft Skills</h2>
+      <h2 className="skills-title">Compétences Relationnelles</h2>
       <div className="gallery">
         {softSkillsData.map((skill, index) => (
-          <div key={index} className="card" tabIndex="0">
-            <h3>{skill.title}</h3>
-            <p>{skill.description}</p>
-          </div>
+          <Link to={skill.link} key={index} className="card-link">
+            <div className="card" tabIndex="0">
+              <h3>{skill.title}</h3>
+              <p>{skill.description}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </section>
@@ -19,3 +22,4 @@ function SoftSkills() {
 }
 
 export default SoftSkills;
+
