2584fa8a5668735b172159c5e5fffc5c2c6fbbf6
supmbh <supmbh@gmail.com>
2024-10-18 01:44:59 +0200

    skills1.3

--- src/components/HardSkills.js
+++ src/components/HardSkills.js
@@ -6,7 +6,7 @@ import hardSkillsData from '../data/hardSkills.json';
 function HardSkills() {
   return (
     <section className="skills-section">
-      <h2 className="skills-title">Compétences Techniques</h2>
+      <h2 className="skills-title">Hard Skills</h2>
       <div className="gallery">
         {hardSkillsData.map((skill, index) => (
           <Link to={skill.link} key={index} className="card-link">
