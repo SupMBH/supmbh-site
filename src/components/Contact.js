bc32479415ad8f6a85f38caf660b6e9e4fef7185
supmbh <supmbh@gmail.com>
2024-10-18 08:47:31 +0200

    contact1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section>
       <h2>Contact</h2>
-      <p>Email : <a href="mailto:supmbh@example.com">supmbh@example.com</a></p>
+      <p>Email : <a href="mailto:supmbh@gmail.com">supmbh@gmail.com</a></p>
       {/* autres moyens  */}
     </section>
   );
