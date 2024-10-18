395653f745cfba198ad40093ea635966c05a80de
supmbh <supmbh@gmail.com>
2024-10-18 03:17:43 +0200

    contact1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <section>
-      <h2>Contact me</h2>
+      <h2>Contact</h2>
       <p>Email : <a href="mailto:supmbh@example.com">supmbh@example.com</a></p>
       {/* autres moyens  */}
     </section>
