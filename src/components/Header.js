6fc6038c8bc60537ead30fbf999018e48ecb4e90
supmbh <supmbh@gmail.com>
2024-10-18 09:12:09 +0200

    MediaQueriesMobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
       <nav>
-        <ul className="nav-links">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <Link to="/hard-skills">Hard Skills</Link>
-          </li>
-          <li>
-            <Link to="/soft-skills">Soft Skills</Link>
-          </li>
-          <li>
-            <a href="/projets">Repositories</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          {/* Autres liens si nécessaire */}
+        <button className="burger-menu" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <li><a href="/">Home</a></li>
+          <li><Link to="/hard-skills">Hard Skills</Link></li>
+          <li><Link to="/soft-skills">Soft Skills</Link></li>
+          <li><a href="/projets">Repositories</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
     </header>
