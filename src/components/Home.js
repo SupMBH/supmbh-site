8e3acdcf26daf413155158c544e840ad4584a7af
supmbh <supmbh@gmail.com>
2024-10-19 17:04:30 +0200

    home3.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import presentationText from '../data/presentationText.json';
 import './Home.css';
 import profileImage from '../assets/images/profile.png';
 
 function Presentation() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleImageClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <section className="presentation-section">
-      <div className="presentation-text">        
+      <div className="presentation-text">
         <p className="bold-text">{presentationText.introduction}</p>
         <p>{presentationText.paragraph1}</p>
         <p>{presentationText.paragraph2}</p>
         <p>{presentationText.conclusion}</p>
       </div>
       <div className="presentation-photo">
-      <img src={profileImage} alt="Profile" />
+        <img
+          src={profileImage}
+          alt="Profile"
+          className={isClicked ? 'clicked' : ''}
+          onClick={handleImageClick}
+        />
       </div>
     </section>
   );
 }
 
 export default Presentation;
-
