48e8f817f2374a782ca007e6677e404d58f453a1
supmbh <supmbh@gmail.com>
2024-10-19 11:31:48 +0200

    contact 1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ function Contact() {
   return (
     <section>
       <h2>Contact</h2>
-      <p>Email : <a href="mailto:supmbh@gmail.com">supmbh@gmail.com</a></p>
-      {/* autres moyens  */}
+      <p>✉️ Email : <a href="mailto:supmbh@gmail.com">supmbh@gmail.com</a></p>      
+      <p>📱 Phone : <a href="tel:0614161402"> 0614161402</a></p>
     </section>
   );
 }
