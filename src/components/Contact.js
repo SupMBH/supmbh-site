99b94aa653b19e3415f819468da548117f84c4a9
SupMBH <supmbh@gmail.com>
2025-01-26 17:01:11 +0100

    newtel

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <h2>Contact</h2>
       <p>✉️ Preferred Email : <a href="mailto:antoine.naget@sfr.fr">antoine.naget@sfr.fr</a></p> 
       <p>✉️ Additional Email (for backup) : <a href="mailto:supmbh@gmail.com">supmbh@gmail.com</a></p>      
-      <p>📱 Phone (please leave a voicemail) : <a href="tel:0614161402"> 0614161402</a></p>
+      <p>📱 Phone (please leave a voicemail) : <a href="tel:0614161402"> +33614161402</a></p>
     </section>
   );
 }
