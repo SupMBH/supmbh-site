c8225ce9ef7b8aebf35b51f3cc6f9b6b31a2e9ef
SupMBH <supmbh@gmail.com>
2026-03-27 19:02:35 +0100

    init nom 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // Remplace 'supmbh-site' par le nom de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/ANsite/',
+  base: '/supmbh-site/',
   build: {
     outDir: 'dist',
     sourcemap: false,
